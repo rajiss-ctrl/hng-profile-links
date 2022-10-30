17fd23a2fbec8a634b604ca3658014dad64505b9
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-10-30 08:49:50 +0100

    dots icon added to the mobile

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import RajissCtrl from '../images/rajiss-ctrl.jpg';
 import Share from '../images/sharebutton.png';
+import Dots from '../images/dots-horizontal.png';
 
 const ProfileImage = () => {
   return (
@@ -10,6 +11,7 @@ const ProfileImage = () => {
           <h1 id='twitter'>Raji S. Sanjo</h1>
           <div id="slack"></div>
           <img className='share' src={Share} alt="share" />
+          <img className='dots' src={Dots} alt="dots horizontal" />
         </div>
     </header>
   )
